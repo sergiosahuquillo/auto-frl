28b64eda3db3cee3ab556585cdc74afd5917b9d6
Sergio Sahuquillo <ssahuquillodev@gmail.com>
2024-12-25 20:06:55 +0100

    Added prudential waiting time to start game before apply frame rate limit

--- main.js
+++ main.js
@@ -8,29 +8,34 @@ const config = checkConfig();
 // Variable to store detected processes
 let detectedProcesses = {};
 
+// Wait for X seconds before updating the frame rate limit
+const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 // Monitoring processes every 5 seconds
 setInterval(async () => {
     const processes = await psList();
     const actualProcesses = {};
 
     // Check active processes
-    config.processes.forEach(process => {
+    for(process of config.processes){
         const activeProcess = processes.find(p => p.name.toLowerCase() === process.name.toLowerCase());
 
         if (activeProcess) {
             if(!detectedProcesses[process.name]){
                 console.log(`Process ${process.name} detected`);
+                await wait(5000);
                 updateFrameRateLimit(process.frl, config.settings);
             }
             actualProcesses[process.name] = true;
         }else{
             if(detectedProcesses[process.name]){
                 console.log(`Process ${process.name} ended`);
+                await wait(5000);
                 updateFrameRateLimit(0, config.settings);
             }
         }
-    });
+    }
 
     // Update the status of detected processes
     detectedProcesses = actualProcesses;
-}, 8000);
+}, 5000);
